4fcecabb91974fab31acf14665e715ee9d42741f
AdiTya-Dev31 <adityadev.31@gmail.com>
2020-10-18 17:51:55 +0530

    Updates

---
+++ precache-manifest.86253e80a4b83ab69b0adfbacd7e8624.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bafc6d4b4ec35dbb82fc763489d6283",
+    "url": "/Orange-coder-react-template2/index.html"
+  },
+  {
+    "revision": "be643c44af8f4e2365f3",
+    "url": "/Orange-coder-react-template2/static/css/main.223ddf8a.chunk.css"
+  },
+  {
+    "revision": "637f1c003651f9cf1743",
+    "url": "/Orange-coder-react-template2/static/js/2.39635143.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Orange-coder-react-template2/static/js/2.39635143.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be643c44af8f4e2365f3",
+    "url": "/Orange-coder-react-template2/static/js/main.d584daae.chunk.js"
+  },
+  {
+    "revision": "39d9abbee5eb311eda26",
+    "url": "/Orange-coder-react-template2/static/js/runtime-main.c18871a8.js"
+  },
+  {
+    "revision": "ad46948005a0f21e4935eed74c63bc2c",
+    "url": "/Orange-coder-react-template2/static/media/Orange-coder-logo-1.ad469480.png"
+  },
+  {
+    "revision": "7aae0755c969e6f79e6ee9150f10d73c",
+    "url": "/Orange-coder-react-template2/static/media/Orange-coder-logo-2.7aae0755.png"
+  },
+  {
+    "revision": "cf422a4d19b1c3f4391c09a8c8989565",
+    "url": "/Orange-coder-react-template2/static/media/back-1.cf422a4d.jpg"
+  },
+  {
+    "revision": "ec1b62530d36332809e61f8b0214255a",
+    "url": "/Orange-coder-react-template2/static/media/back-2.ec1b6253.jpg"
+  },
+  {
+    "revision": "e70e4e8e659ba91e625d01ed000ca9bf",
+    "url": "/Orange-coder-react-template2/static/media/back-3.e70e4e8e.jpg"
+  },
+  {
+    "revision": "2fd31a064402b3ba8a209d130c9dac33",
+    "url": "/Orange-coder-react-template2/static/media/back-4.2fd31a06.jpg"
+  },
+  {
+    "revision": "1ecc658ea27153b9a728803e30ed4e04",
+    "url": "/Orange-coder-react-template2/static/media/back-5.1ecc658e.jpg"
+  },
+  {
+    "revision": "3cc75493e5d2dbe22bc35511ff9bd991",
+    "url": "/Orange-coder-react-template2/static/media/serv-img-1.3cc75493.jpg"
+  },
+  {
+    "revision": "8c27868549d3b79ec80f8bca0b5efb16",
+    "url": "/Orange-coder-react-template2/static/media/serv-img-2.8c278685.jpg"
+  },
+  {
+    "revision": "ebaec89b451a56eb800f8ffdd14debfe",
+    "url": "/Orange-coder-react-template2/static/media/serv-img-3.ebaec89b.jpg"
+  },
+  {
+    "revision": "e6b5de8e170a2e8acaec98979d01feb6",
+    "url": "/Orange-coder-react-template2/static/media/serv-img-4.e6b5de8e.jpg"
+  },
+  {
+    "revision": "5a3f7a42ae7d171cb958c9edfcfa6cd8",
+    "url": "/Orange-coder-react-template2/static/media/serv-img-5.5a3f7a42.jpg"
+  }
+]);
